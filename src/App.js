8ee2765f33034eb29cc5482038617445f06a02f7
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2022-07-15 11:15:12 +0200

    added the configuration files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-[80rem] bg-[black]">
+      <h1 className='text-pink-700'>hey three</h1>
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Snavbar from './Snavbar'
+
+const Hero = () => {
+  return (
+    <div>
+      <Snavbar />
+    </div>
+  )
+}
+
+export default Hero
